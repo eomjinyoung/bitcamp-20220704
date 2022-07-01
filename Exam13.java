9b5519eecbae03461b3d53e07dedd25bc8068be9
eomjinyoung <jinyoung.eom@gmail.com>
2022-07-01 13:03:07 +0900

    .

---
+++ Exam13.java
@@ -0,0 +1,10 @@
+public class Exam13 {
+    
+    static float f = 12.375f;
+    static double f2 = 12.375;
+
+    public static void main(String[] args) {
+        System.out.println(f);
+        System.out.println(f2);
+    }
+}
