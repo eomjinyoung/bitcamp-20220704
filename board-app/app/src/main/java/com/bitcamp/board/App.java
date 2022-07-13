7fe1518acc4554a41881fc1d913e4e2efab05f66
eomjinyoung <jinyoung.eom@gmail.com>
2022-07-13 15:05:43 +0900

    .

--- board-app/app/src/main/java/com/bitcamp/board/App.java
+++ board-app/app/src/main/java/com/bitcamp/board/App.java
@@ -14,13 +14,17 @@ public class App {
 
     java.util.Scanner keyboardInput = new java.util.Scanner(System.in);
 
-    int no = 0;
-    String title = "";
-    String content = "";
-    String writer = "";
-    String password = "";
-    int viewCount = 0;
-    long createdDate = 0;
+    final int SIZE = 3;
+
+    int[] no = new int[SIZE];
+    String[] title = new String[SIZE];
+    String[] content = new String[SIZE];
+    String[] writer = new String[SIZE];
+    String[] password = new String[SIZE];
+    int[] viewCount = new int[SIZE];
+    long[] createdDate = new long[SIZE];
+
+    int boardCount = 0; // 저장된 게시글의 개수
 
     while (true) {
       System.out.println("메뉴:");
@@ -33,8 +37,10 @@ public class App {
       int menuNo = keyboardInput.nextInt();
       keyboardInput.nextLine(); // 입력한 숫자 뒤에 남아 있는 줄바꿈 코드 제거
 
+      
       if (menuNo == 0) {
         break;
+
       } else if (menuNo == 1) {
         System.out.println("[게시글 목록]");
         System.out.println("번호 제목 조회수 작성자 등록일");
@@ -55,41 +61,55 @@ public class App {
 
         System.out.printf("%d\t%s\t%d\t%s\t%s\n",
             4, "제목입니다4", 45, "유관순", "2022-07-08");
+
       } else if (menuNo == 2) {
         System.out.println("[게시글 상세보기]");
 
-        System.out.printf("번호: %d\n", no);
-        System.out.printf("제목: %s\n", title);
-        System.out.printf("내용: %s\n", content);
-        System.out.printf("조회수: %d\n", viewCount);
-        System.out.printf("작성자: %s\n", writer);
-        java.util.Date date = new java.util.Date(createdDate);
+        System.out.print("조회할 게시글 번호? ");
+        String input = keyboardInput.nextLine();
+        int boardNo = Integer.parseInt(input);
+
+        System.out.printf("번호: %d\n", no[boardNo]);
+        System.out.printf("제목: %s\n", title[boardNo]);
+        System.out.printf("내용: %s\n", content[boardNo]);
+        System.out.printf("조회수: %d\n", viewCount[boardNo]);
+        System.out.printf("작성자: %s\n", writer[boardNo]);
+        java.util.Date date = new java.util.Date(createdDate[boardNo]);
         System.out.printf("등록일: %tY-%1$tm-%1$td %1$tH:%1$tM\n", date);
       
       } else if (menuNo == 3) {
         System.out.println("[게시글 등록]");
 
         System.out.print("제목? ");
-        title = keyboardInput.nextLine();
+        title[boardCount] = keyboardInput.nextLine();
 
         System.out.print("내용? ");
-        content = keyboardInput.nextLine();
+        content[boardCount] = keyboardInput.nextLine();
 
         System.out.print("작성자? "); 
-        writer = keyboardInput.nextLine();
+        writer[boardCount] = keyboardInput.nextLine();
 
         System.out.print("암호? ");
-        password = keyboardInput.nextLine();
+        password[boardCount] = keyboardInput.nextLine();
 
-        no = 1;
-        viewCount = 0;
-        createdDate = System.currentTimeMillis();
+        /*
+        if (boardCount == 0) {
+          no[boardCount] = 1;
+        } else {
+          no[boardCount] = no[boardCount - 1] + 1;
+        }
+        */
+        no[boardCount] = boardCount == 0 ? 1 : no[boardCount - 1] + 1;
 
+        viewCount[boardCount] = 0;
+        createdDate[boardCount] = System.currentTimeMillis();
         
+        boardCount++;
+
       } else {
         System.out.println("메뉴 번호가 옳지 않습니다!");
       }
-
+      
       System.out.println(); // 메뉴를 처리한 후 빈 줄 출력
     } // while
 
