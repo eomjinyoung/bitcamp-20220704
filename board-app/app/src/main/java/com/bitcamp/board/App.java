05b991324a2a210b85fb1e111e4b1fa8ebde2182
eomjinyoung <jinyoung.eom@gmail.com>
2022-07-13 16:06:26 +0900

    .

--- board-app/app/src/main/java/com/bitcamp/board/App.java
+++ board-app/app/src/main/java/com/bitcamp/board/App.java
@@ -37,7 +37,7 @@ public class App {
       int menuNo = keyboardInput.nextInt();
       keyboardInput.nextLine(); // 입력한 숫자 뒤에 남아 있는 줄바꿈 코드 제거
 
-      
+      System.out.println("--------------------------------------");
       if (menuNo == 0) {
         break;
 
@@ -45,23 +45,24 @@ public class App {
         System.out.println("[게시글 목록]");
         System.out.println("번호 제목 조회수 작성자 등록일");
 
-        System.out.print(1);
-        System.out.print("\t");
-        System.out.print("제목입니다1");
-        System.out.print('\t');
-        System.out.print(20 + "\t");
-        System.out.print("홍길동\t");
-        System.out.print("2022-07-08\r\n");
+        // 날짜 정보에서 값을 추출하여 특정 포맷의 문자열로 만들어줄 도구를 준비
+        java.text.SimpleDateFormat formatter = 
+          new java.text.SimpleDateFormat("yyyy-MM-dd");
+        
+        int i = 0;
+        while (i < boardCount) {
+          // 밀리초 데이터 ==> Date 도구함으로 날짜 정보를 설정
+          java.util.Date date = new java.util.Date(createdDate[i]);
+          
+          // 날짜 정보 ==> "yyyy-MM-dd" 형식의 문자열
+          String dateStr = formatter.format(date); 
 
-        System.out.print(2 + "\t" + "제목입니다2\t" +
-            11 + "\t" + "홍길동\t" + "2022-07-08\n");
-
-        System.out.println(3 + "\t제목입니다3\t" +
-            31 + "\t" + "임꺽정\t2022-07-08");
-
-        System.out.printf("%d\t%s\t%d\t%s\t%s\n",
-            4, "제목입니다4", 45, "유관순", "2022-07-08");
-
+          System.out.printf("%d\t%s\t%d\t%s\t%s\n",
+            no[i], title[i], viewCount[i], writer[i], dateStr);
+          
+          i++; // 배열 인덱스를 증가시킨다
+        }
+        
       } else if (menuNo == 2) {
         System.out.println("[게시글 상세보기]");
 
@@ -69,16 +70,39 @@ public class App {
         String input = keyboardInput.nextLine();
         int boardNo = Integer.parseInt(input);
 
-        System.out.printf("번호: %d\n", no[boardNo]);
-        System.out.printf("제목: %s\n", title[boardNo]);
-        System.out.printf("내용: %s\n", content[boardNo]);
-        System.out.printf("조회수: %d\n", viewCount[boardNo]);
-        System.out.printf("작성자: %s\n", writer[boardNo]);
-        java.util.Date date = new java.util.Date(createdDate[boardNo]);
+        // 해당 번호의 게시글이 몇 번 배열에 들어 있는지 알아내기
+        int boardIndex = -1;
+        int i = 0;
+        while (i < boardCount) {
+          if (no[i] == boardNo) {
+            boardIndex = i;
+            break;
+          }
+          i++;
+        }
+
+        // 사용자가 입력한 번호에 해당하는 게시글을 못 찾았다면
+        if (boardIndex == -1) {
+          System.out.println("해당 번호의 게시글이 없습니다!");
+          continue;
+        }
+
+        System.out.printf("번호: %d\n", no[boardIndex]);
+        System.out.printf("제목: %s\n", title[boardIndex]);
+        System.out.printf("내용: %s\n", content[boardIndex]);
+        System.out.printf("조회수: %d\n", viewCount[boardIndex]);
+        System.out.printf("작성자: %s\n", writer[boardIndex]);
+        java.util.Date date = new java.util.Date(createdDate[boardIndex]);
         System.out.printf("등록일: %tY-%1$tm-%1$td %1$tH:%1$tM\n", date);
       
       } else if (menuNo == 3) {
         System.out.println("[게시글 등록]");
+
+        // 배열의 크기를 초과하지 않았는지 검사한다
+        if (boardCount == SIZE) {
+          System.out.println("게시글을 더이상 저장할 수 없습니다.");
+          continue;
+        }
 
         System.out.print("제목? ");
         title[boardCount] = keyboardInput.nextLine();
