e7f47e7bb52b69a014b73b9a91421c234e6983bc
eomjinyoung <jinyoung.eom@gmail.com>
2022-08-02 11:01:36 +0900

    .

--- board-app/app/README.md
+++ board-app/app/README.md
@@ -1,28 +1,15 @@
-# 021. 상속 대신 연관공통 코드(필드,메서드)를 공유하는 방법 : 연관(association)
+# 022. 자료구조를 구현하고 다루는 방법: Linked List 구현 및 사용
 
 
 ## 작업 내용
 
-### 0단계 - 프로젝트 소스 준비
+### 1단계 - LinkedList 를 구현한다.
 
-- 019 버전 소스를 가져온다.
+- 020 버전에서 구현한 LinkedList 관련 클래스를 가져온다.
 
-### 1단계 - ObjectList를 상속하는 대신 연관(포함) 관계로 바꾼다.
+### 2단계 - BoardDao와 MemberDao는 ObjectList 대신 LinkedList를 사용한다.
 
-- com.bitcamp.board.dao.BoardList 클래스 변경
-  - ObjectList를 포함하는 관계로 바꾼다.
-  - 기능을 ObjectList 로 위임할 수 있도록 메서드를 추가, 변경한다.
-  - 서브 클래스가 아닌 관계로 클래스 이름을 BoardDao로 변경한다.
-  - 메서드 이름도 데이터를 다루는 이름으로 적절하게 변경한다.
-    - 기존의 ObjectList 는 인스턴스를 다룬다는 의미로 메서드 이름도 그에 맞춰서 지었다.
-    - BoardDao은 인스턴스라는 기술적인 용어 대신 데이터에 초점을 맞춘다.
-    - 따라서 데이터에 초점을 맞춘 메서드 이름으로 변경한다.
-- com.bitcamp.board.dao.MemberList 클래스 변경
-  - BoardList를 변경한 것과 똑같이 처리한다.
-
-### 2단계 - BoardDao와 MemberDao에 맞춰 XxxHandler를 변경한다.
-
-- com.bitcamp.board.handler.BoardHandler 클래스 변경
-  - BoardList 대신 ObjectList에 의존하고 있는 BoardDao를 사용한다.
-- com.bitcamp.board.handler.MemberHandler 클래스 변경
-  - MemberList 대신 ObjectList에 의존하고 있는 MemberDao를 사용한다.
+- com.bitcamp.board.dao.BoardDao 클래스 변경
+  - 의존 객체를 ObjectList 에서 LinkedList로 교체한다.
+- com.bitcamp.board.dao.MemberDao 클래스 변경
+  - 의존 객체를 ObjectList 에서 LinkedList로 교체한다.
---
+++ board-app/app/src/main/java/com/bitcamp/util/LinkedList.java
@@ -0,0 +1,141 @@
+package com.bitcamp.util;
+
+/**
+ * Node를 이용해 값을 목록을 관리하는 일을 한다.
+ * 
+ * @author bitcamp
+ *
+ */
+public class LinkedList {
+
+  private Node head; // 첫 노드의 주소를 저장
+  private Node tail; // 마지막 노드의 주소를 저장
+  private int size; // 저장된 데이터의 개수
+
+  /**
+   * 파라미터로 주어진 값을 노드에 담아 리스트 끝에 연결한다.
+   * @param value
+   */
+  public void append(Object value) {
+    // Node 생성 후 값을 저장한다.
+    Node node = new Node(value);
+
+    size++; // 목록의 크기를 한 개 증가시킨다.
+
+    // 리스트의 끝에 노드를 붙인다.
+    // 만약, 리스트에 노드가 없다면 
+    if (tail == null) {
+      head = tail = node; // 첫 노드를 등록한다.
+      return;
+    }
+
+
+    tail.next = node; // 리스트 끝에 새 노드를 연결한다.
+    node.prev = tail; // 새 노드가 현재의 끝 노드를 가리키게 한다. 
+
+    tail = node; // 새 노드를 끝 노드로 만든다.
+  }
+
+  public Object retrieve(int index) {
+
+    // 인덱스의 유효 여부 검사
+    if (index < 0 || index >= size) {
+      throw new ListException("인덱스의 범위를 초과했습니다!");
+    }
+
+    // 인덱스에 해당하는 노드를 찾을 때 head 부터 시작한다. 
+    Node cursor = head;
+
+    // 지정된 인덱스의 노드 주소를 알아낸다.
+    for (int i = 0; i < index; i++) {
+      cursor = cursor.next;
+    }
+
+    // cursor가 가리키는 노드의 값을 꺼내 리턴한다.
+    return cursor.value;
+  }
+
+  public Object delete(int index) {
+
+    // 인덱스의 유효 여부 검사
+    if (index < 0 || index >= size) {
+      throw new ListException("인덱스의 범위를 초과했습니다!");
+    }
+
+    // 목록 크기를 한 개 줄인다.
+    size--;
+
+    // 삭제할 값을 임시 보관하여 메서드를 리턴할 때 호출자에게 전달한다.
+    Object deleted;
+
+    if (head == tail) { // 마지막 남은 노드를 제거할 때
+      deleted = head.value; // 노드를 삭제하기 전에 리턴할 수 있도록 값을 임시 보관한다.
+      head.value = null; // 노드에 들어 있는 값 객체의 주소를 비운다.
+      head = tail = null;
+      return deleted; // 메서드를 종료할 때 호출자에게 삭제한 값을 리턴한다.
+    }
+
+    // 삭제할 노드를 찾기 위해 시작 노드를 head로 설정한다.
+    Node cursor = head;
+
+    // 지정된 인덱스의 노드 주소를 알아낸다.
+    for (int i = 0; i < index; i++) {
+      cursor = cursor.next;
+    }
+
+    // 찾은 노드의 앞, 뒤 노드를 바로 연결한다.
+    if (cursor.prev != null) { // 맨 앞 노드가 아니라면
+      cursor.prev.next = cursor.next; // 현재 노드의 다음 노드 주소를 이전 노드의 next 저장
+    } else { // 맨 앞 노드라면
+      head = cursor.next; // 삭제할 다음 노드를 시작 노드로 설정한다. 
+      head.prev = null; // 시작 노드이기에 앞노드를 가리키지 않게 한다.
+    }
+
+    if (cursor.next != null) { // 마지막 노드가 아니라면
+      cursor.next.prev = cursor.prev; // 현재 노드의 이전 노드 주소를 다음 노드의 prev 저장
+    } else { // 마지막 노드라면 
+      tail = cursor.prev; // 현재 커서의 이전 노드를 마지막 노드로 설정한다.
+      tail.next = null; // 마지막 노드이기에 다음 노드를 가리키지 않게 한다.
+    }
+
+    // 삭제할 노드를 초기화시킨다.
+    // => garbage 객체가 다른 garbage 객체를 참조하지 않게 한다.
+    deleted = cursor.value; // 노드를 삭제하기 전에 노드에 들어 있는 값을 임시 보관해 둔다.
+    cursor.value = null;
+    cursor.prev = null;
+    cursor.next = null;
+
+    return deleted; // 메서드를 리턴할 때 삭제된 값을 호출자에게 전달한다.
+  }
+
+  public int length() {
+    return size;
+  }
+
+  public Object[] getArray() {
+    // 값을 담을 배열을 준비
+    Object[] arr = new Object[size];
+
+    // 노드를 따라 가면서 값을 꺼내 배열에 담는다.
+    Node cursor = head;
+    for (int i = 0; i < size; i++) {
+      arr[i] = cursor.value;
+      cursor = cursor.next;
+    }
+
+    return arr;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
