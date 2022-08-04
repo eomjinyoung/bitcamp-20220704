794ae419e6ac86f5d2e80779521eec1ecec72047
eomjinyoung <jinyoung.eom@gmail.com>
2022-08-04 16:13:47 +0900

    .

---
+++ board-app/app-031/README.md
@@ -0,0 +1,14 @@
+# 031. 기존 List 구현체를 자바 컬렉션 API로 교체하기: java.util 패키지의 클래스 사용
+
+## 작업 내용
+
+### 1단계 - 기존의 목록 관련 클래스를 자바 컬렉션 API로 교체한다.
+
+- com.bitcamp.util.List 인터페이스 삭제
+- com.bitcamp.util.AbstractList 추상 클래스 삭제
+- com.bitcamp.util.ObjectList 클래스 삭제
+- com.bitcamp.util.LinkedList 클래스 삭제
+- com.bitcamp.util.Stack 클래스 삭제
+- com.bitcamp.board.dao.BoardDao 클래스 변경
+- com.bitcamp.board.dao.MemberDao 클래스 변경
+- com.bitcamp.board.App 클래스 변경
---
+++ board-app/app-031/src/main/java/com/bitcamp/board/dao/MemberDao.java
@@ -0,0 +1,54 @@
+package com.bitcamp.board.dao;
+
+import java.util.LinkedList;
+import java.util.List;
+import com.bitcamp.board.domain.Member;
+
+// 회원 목록을 관리하는 역할
+//
+public class MemberDao {
+
+  List<Member> list = new LinkedList<Member>();
+
+  public void insert(Member member) {
+    list.add(member);
+  }
+
+  public Member findByEmail(String email) {
+    for (int i = 0; i < list.size(); i++) {
+      Member member = list.get(i);
+      if (member.email.equals(email)) {
+        return member;
+      }
+    }
+    return null;
+  }
+
+  public boolean delete(String email) {
+    for (int i = 0; i < list.size(); i++) {
+      Member member = list.get(i);
+      if (member.email.equals(email)) {
+        return list.remove(i) != null;
+      }
+    }
+    return false;
+  }
+
+  public Member[] findAll() {
+    return list.toArray(new Member[0]);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
