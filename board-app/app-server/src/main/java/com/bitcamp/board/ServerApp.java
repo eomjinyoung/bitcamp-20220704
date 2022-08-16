6511c6f132b7cb233c02c9dae0b2192c4e14b05d
eomjinyoung <jinyoung.eom@gmail.com>
2022-08-16 12:22:24 +0900

    .

--- board-app/app-server/src/main/java/com/bitcamp/board/ServerApp.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/ServerApp.java
@@ -1,5 +1,7 @@
 package com.bitcamp.board;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
@@ -30,8 +32,22 @@ public class ServerApp {
       // => 클라이언트가 보낸 데이터를 읽을 때 사용할 도구를 준비한다.
       InputStream in = socket.getInputStream();
 
+      // => 데이터를 읽을 때 primitive type 또는 String 타입의 값을 
+      //    보다 손쉽게 읽을 수 있도록 기존의 입력 도구에 보조 도구(decorator)붙여 사용한다.
+      DataInputStream in2 = new DataInputStream(in);
+
+
       // => 클라이언트로 데이터를 보낼 때 사용할 도구를 준비한다.
       OutputStream out = socket.getOutputStream();
+
+      // => 데이터를 출력할 때 primitive type 또는 String 타입의 값을 
+      //    보다 손쉽게 출력 할 수 있도록 기존의 출력 도구에 보조 도구(decorator)붙여 사용한다.
+      DataOutputStream out2 = new DataOutputStream(out);
+
+
+      // 클라이언트와 서버 사이에 정해진 규칙(protocol)에 따라 데이터를 주고 받는다.
+
+
 
       // 네트워크 종료
       // => 더이상 클라이언트와 연결하고 싶지 않다면 네트워크를 종료한다.
