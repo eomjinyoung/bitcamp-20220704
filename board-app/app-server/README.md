144a839a2500db86fa303f63a8e53c6a5c4a9325
eomjinyoung <jinyoung.eom@gmail.com>
2022-08-25 12:08:27 +0900

    .

--- board-app/app-server/README.md
+++ board-app/app-server/README.md
@@ -10,3 +10,24 @@
 
 - com.bitcamp.board.ServerApp 클래스 변경
 
+### 3단계 - 리팩토링1: RequestThread를 ServerApp의 static nested class로 만든다.
+
+- com.bitcamp.board.RequestThread 클래스 삭제
+  - 삭제 전 클래스 백업: RequestThread.java.01
+- com.bitcamp.board.ServerApp 클래스 변경
+  - 변경 전 클래스 백업: ServerApp.java.01
+
+### 4단계 - 리팩토링2: RequestThread를 main()의 local class로 만든다.
+
+- com.bitcamp.board.ServerApp 클래스 변경
+  - 변경 전 클래스 백업: ServerApp.java.02
+
+### 5단계 - 리팩토링3: RequestThread를 Runnable 구현체로 만든다.
+
+- com.bitcamp.board.ServerApp 클래스 변경
+  - 변경 전 클래스 백업: ServerApp.java.03
+
+### 6단계 - 리팩토링4: RequestRunnable을 익명 클래스로 만든다.
+
+- com.bitcamp.board.ServerApp 클래스 변경
+  - 변경 전 클래스 백업: ServerApp.java.04
--- board-app/app-server/src/main/java/com/bitcamp/board/ServerApp.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/ServerApp.java
@@ -1,5 +1,7 @@
 package com.bitcamp.board;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Hashtable;
@@ -8,31 +10,52 @@ import com.bitcamp.board.servlet.MemberServlet;
 import com.bitcamp.servlet.Servlet;
 
 public class ServerApp {
+
   public static void main(String[] args) {
+
+    // 클라이언트 요청을 처리할 객체 준비
+    Hashtable<String,Servlet> servletMap = new Hashtable<>();
+    servletMap.put("board", new BoardServlet("board"));
+    servletMap.put("reading", new BoardServlet("reading"));
+    servletMap.put("visit", new BoardServlet("visit"));
+    servletMap.put("notice", new BoardServlet("notice"));
+    servletMap.put("daily", new BoardServlet("daily"));
+    servletMap.put("member", new MemberServlet("member"));
+
     System.out.println("[게시글 데이터 관리 서버]");
 
     try (ServerSocket serverSocket = new ServerSocket(8888);) {
 
       System.out.println("서버 소켓 준비 완료!");
 
-      // 클라이언트 요청을 처리할 객체 준비
-      Hashtable<String,Servlet> servletMap = new Hashtable<>();
-      servletMap.put("board", new BoardServlet("board"));
-      servletMap.put("reading", new BoardServlet("reading"));
-      servletMap.put("visit", new BoardServlet("visit"));
-      servletMap.put("notice", new BoardServlet("notice"));
-      servletMap.put("daily", new BoardServlet("daily"));
-      servletMap.put("member", new MemberServlet("member"));
-
       while (true) {
-        // 클라이언트가 연결되면,
-        Socket socket = serverSocket.accept();
+        new Thread(new Runnable() {
+          Socket socket = serverSocket.accept();
+          @Override
+          public void run() {
+            try (Socket socket = this.socket;
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
 
-        // 클라이언트 요청을 처리할 스레드를 만든다.
-        RequestThread t = new RequestThread(socket, servletMap);
+              System.out.println("클라이언트와 연결 되었음!");
 
-        // main 실행 흐름에서 분리하여 별도의 실행 흐름으로 작업을 수행시킨다.
-        t.start();
+              String dataName = in.readUTF();
+
+              Servlet servlet = servletMap.get(dataName);
+              if (servlet != null) {
+                servlet.service(in, out);
+              } else {
+                out.writeUTF("fail");
+              }
+
+              System.out.println("클라이언트와 연결을 끊었음!");
+
+            } catch (Exception e) {
+              System.out.println("클라이언트 요청 처리 중 오류 발생!");
+              e.printStackTrace();
+            }
+          }
+        }).start();
       }
     } catch (Exception e) {
       e.printStackTrace();
@@ -40,4 +63,8 @@ public class ServerApp {
 
     System.out.println("서버 종료!");
   }
+
+
+
+
 }
