2fef9c6b45bc62b9e27f294e4a28467dc6971677
eomjinyoung <jinyoung.eom@gmail.com>
2022-09-26 12:58:02 +0900

    .

---
+++ board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardAddController.java
@@ -0,0 +1,50 @@
+package com.bitcamp.board.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.bitcamp.board.dao.BoardDao;
+import com.bitcamp.board.domain.Board;
+
+@WebServlet("/board/add")
+public class BoardAddController extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  BoardDao boardDao;
+
+  @Override
+  public void init() {
+    boardDao = (BoardDao) this.getServletContext().getAttribute("boardDao");
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    try {
+      Board board = new Board();
+      board.title = request.getParameter("title");
+      board.content = request.getParameter("content");
+      board.memberNo = Integer.parseInt(request.getParameter("writerNo"));
+
+      if (boardDao.insert(board) == 0) {
+        throw new Exception("게시글 등록 실패!");
+      }
+
+      response.setContentType("text/html;charset=UTF-8");
+      request.getRequestDispatcher("/board/add.jsp").include(request, response); 
+
+    } catch (Exception e) {
+      request.setAttribute("exception", e);
+      request.getRequestDispatcher("/error.jsp").forward(request, response);
+    }
+  }
+}
+
+
+
+
+
+
