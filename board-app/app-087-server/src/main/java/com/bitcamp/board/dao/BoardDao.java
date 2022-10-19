5b5248b2d5627077868f13200ecdea2f59fcd21f
eomjinyoung <jinyoung.eom@gmail.com>
2022-10-19 16:00:25 +0900

    .

---
+++ board-app/app-087-server/src/main/java/com/bitcamp/board/dao/BoardDao.java
@@ -0,0 +1,46 @@
+package com.bitcamp.board.dao;
+
+import java.util.List;
+import com.bitcamp.board.domain.AttachedFile;
+import com.bitcamp.board.domain.Board;
+
+public interface BoardDao {
+
+  int insert(Board board);
+
+  Board findByNo(int no);
+
+  int update(Board board);
+
+  int delete(int no);
+
+  int deleteByMember(int memberNo);
+
+  List<Board> findAll();
+
+  int insertFiles(Board board);
+
+  AttachedFile findFileByNo(int fileNo);
+
+  List<AttachedFile> findFilesByBoard(int boardNo);
+
+  int deleteFile(int fileNo);
+
+  int deleteFiles(int boardNo);
+
+  int deleteFilesByMemberBoards(int memberNo);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- board-app/app-server/README.md
+++ board-app/app-server/README.md
@@ -1,14 +1,11 @@
-### 087. 프로젝트에 서블릿 컨테이너(예:톰캣) 삽입하기
+### 088. SpringBoot 적용하기
 
 ## 작업 내용
 
-### 1단계 - 삽입 용 톰캣 서버 라이브러리를 프로젝트에 추가한다.
+### 1단계 - spring.io 사이트에서 SpringBoot 프로젝트 템플릿을 생성한다.
 
-- search.maven.org 에서 'tomcat-embed-jasper' 검색
-- 빌드 스크립트 파일(build.gradle)에 등록
-- `gradle eclipse` 실행해서 이클립스IDE 설정 파일을 갱신
-- 이클립스IDE에서 'refresh'
+- SpringBoot 기본 프로젝트를 생성한다.
 
-### 2단계 - 톰캣 서버를 실행하는 메인 클래스를 정의한다.
+### 2단계 - SpringBoot 설정을 프로젝트에 반영한다.
 
-- com.bitcamp.board.App 클래스 생성
+- build.gradle 변경
